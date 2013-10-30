663f207e0346d7271ddd5b728b841520831db484
Martin Thompson <mjpt777@gmail.com>
2013-10-30 08:19:31 +0000

    Cache return of nested type for efficiency rather than resetting against buffer.

--- perf/java/uk/co/real_logic/sbe/CarBenchmark.java
+++ perf/java/uk/co/real_logic/sbe/CarBenchmark.java
@@ -16,15 +16,15 @@
 
 package uk.co.real_logic.sbe;
 
-import java.nio.ByteBuffer;
-
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
-
 import uk.co.real_logic.sbe.examples.Car;
+import uk.co.real_logic.sbe.examples.Engine;
 import uk.co.real_logic.sbe.examples.Model;
 import uk.co.real_logic.sbe.generation.java.DirectBuffer;
+
+import java.nio.ByteBuffer;
 
 public class CarBenchmark
 {
@@ -49,14 +49,19 @@ public class CarBenchmark
         state.car.fuelFigures().mpg(35.6F);
         state.car.fuelFigures().speed(100);       
         state.car.modelYear(2005);
-        state.car.performanceFigures().accelerationSize(10);
-        state.car.performanceFigures().octaneRating((short) 98);
+
+        final Car.PerformanceFigures performanceFigures = state.car.performanceFigures();
+        performanceFigures.accelerationSize(10);
+        performanceFigures.octaneRating((short)98);
+
         state.car.putMake(AUDI, 0, AUDI.length);
         state.car.putModel(R8, 0, R8.length);
-        state.car.engine().capacity(8000);
-        state.car.engine().fuel(5);
-        state.car.engine().putManufacturerCode(V8, 0, V8.length);
-        state.car.engine().numCylinders((short) 8);
+
+        final Engine engine = state.car.engine();
+        engine.capacity(8000);
+        engine.fuel(5);
+        engine.putManufacturerCode(V8, 0, V8.length);
+        engine.numCylinders((short)8);
         
         return state.car;
     }
