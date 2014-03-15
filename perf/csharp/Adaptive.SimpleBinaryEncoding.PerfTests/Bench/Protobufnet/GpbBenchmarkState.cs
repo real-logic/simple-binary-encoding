e84b58220648e9d426c6c4d0fa4227aef6c12bae
MendelMonteiro <mendel.monteiro@gmail.com>
2014-03-15 22:30:50 +0100

    [C#] Added protobuf-net performance tests

---
+++ perf/csharp/Adaptive.SimpleBinaryEncoding.PerfTests/Bench/Protobufnet/GpbBenchmarkState.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace Adaptive.SimpleBinaryEncoding.PerfTests.Bench.Protobufnet
+{
+    public class GpbBenchmarkState
+    {
+        public GpbBenchmarkState()
+        {
+            Marketdata = CreateSampleData();
+            var input = new MemoryStream();
+            Out = input;
+            In = input;
+
+            var model = TypeModel.Create();
+            Type type = typeof(MarketDataIncrementalRefreshTrades);
+            model.Deserialize(Out, null, type);
+            Out.Position = 0;
+            Compiled = model.Compile();
+
+            Writer = new ProtoWriter(Out, model, null);
+            Reader = new ProtoReader(In, model, null);
+
+            GpbMarketDataBenchmark.Encode(Marketdata, input, Compiled, Writer);
+        }
+
+        public TypeModel Compiled { get; set; }
+        public MarketDataIncrementalRefreshTrades Marketdata { get; set; }
+        public Stream In { get; private set; }
+        public MemoryStream Out { get; private set; }
+        public ProtoWriter Writer { get; private set; }
+        public ProtoReader Reader { get; private set; }
+
+        private static MarketDataIncrementalRefreshTrades CreateSampleData()
+        {
+            MarketDataIncrementalRefreshTrades trades = new MarketDataIncrementalRefreshTrades();
+            trades.transactTime = 1234L;
+            trades.eventTimeDelta = 987;
+            trades.matchEventIndicator = MarketDataIncrementalRefreshTrades.MatchEventIndicator.END_EVENT;
+
+            MdIncGrp groupBuilder = new MdIncGrp();
+            groupBuilder.tradeId = 1234L;
+            groupBuilder.securityId = 56789L;
+            groupBuilder.mdEntryPx = new Decimal64();
+            groupBuilder.mdEntryPx.mantissa = 50;
+            groupBuilder.mdEntrySize = new IntQty32();
+            groupBuilder.mdEntrySize.mantissa = 10;
+            groupBuilder.numberOfOrders = 1;
+            groupBuilder.mdUpdateAction = MdIncGrp.MdUpdateAction.NEW;
+            groupBuilder.repSeq = 1;
+            groupBuilder.aggressorSide = MdIncGrp.Side.BUY;
+            groupBuilder.mdEntryType = MdIncGrp.MdEntryType.BID;
+            trades.mdIncGroup.Add(groupBuilder);
+
+            groupBuilder = new MdIncGrp();
+            groupBuilder.tradeId = 1234L;
+            groupBuilder.securityId = 56789L;
+            groupBuilder.mdEntryPx = new Decimal64();
+            groupBuilder.mdEntryPx.mantissa = 50;
+            groupBuilder.mdEntrySize = new IntQty32();
+            groupBuilder.mdEntrySize.mantissa = 10;
+            groupBuilder.numberOfOrders = 1;
+            groupBuilder.mdUpdateAction = MdIncGrp.MdUpdateAction.NEW;
+            groupBuilder.repSeq = 1;
+            groupBuilder.aggressorSide = MdIncGrp.Side.SELL;
+            groupBuilder.mdEntryType = MdIncGrp.MdEntryType.OFFER;
+            trades.mdIncGroup.Add(groupBuilder);
+            return trades;
+        }
+    }
+}
---
+++ perf/csharp/Adaptive.SimpleBinaryEncoding.PerfTests/Bench/Protobufnet/GpbMarketDataBenchmark.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace Adaptive.SimpleBinaryEncoding.PerfTests.Bench.Protobufnet
+{
+    public static class GpbMarketDataBenchmark
+    {
+        public static long Encode(MarketDataIncrementalRefreshTrades trades, Stream output, TypeModel compiled, ProtoWriter writer)
+        {
+            compiled.Serialize(writer, trades);
+            
+            return output.Position;
+        }
+
+        public static long Decode(MarketDataIncrementalRefreshTrades marketData,
+            Stream input, TypeModel compiled, ProtoReader reader)
+        {
+            marketData.mdIncGroup.Clear();
+            compiled.Deserialize(reader, marketData, typeof(MarketDataIncrementalRefreshTrades));
+
+            ulong transactTime = marketData.transactTime;
+            uint eventTimeDelta = marketData.eventTimeDelta;
+            MarketDataIncrementalRefreshTrades.MatchEventIndicator matchEventIndicator = marketData.matchEventIndicator;
+
+            foreach (MdIncGrp mdIncGrp in marketData.mdIncGroup)
+            {
+                ulong tradeId = mdIncGrp.tradeId;
+                ulong securityId = mdIncGrp.securityId;
+                long mantissa = mdIncGrp.mdEntryPx.mantissa;
+                int i = mdIncGrp.mdEntrySize.mantissa;
+                uint numberOfOrders = mdIncGrp.numberOfOrders;
+                MdIncGrp.MdUpdateAction mdUpdateAction = mdIncGrp.mdUpdateAction;
+                uint repSeq = mdIncGrp.repSeq;
+                MdIncGrp.Side aggressorSide = mdIncGrp.aggressorSide;
+                MdIncGrp.MdEntryType mdEntryType = mdIncGrp.mdEntryType;
+            }
+
+            return input.Length;
+        }
+    }
+}
