2def7a5fda4566fe28605357f10c22aea559a261
Claudiu Soroiu <csoroiu@gmail.com>
2014-06-19 17:28:09 +0300

    [Java] 1. Moved BitUtil to the u.c.r.sbe.codec.java package (it is related more to the java codec than the util package) 2. Moved JavaUtil to u.c.r.sbe.generation.java (apparently each generator has its own Util class) 3. Code tidy for protobuf based MarketDataBenchmark

--- main/java/uk/co/real_logic/sbe/generation/java/JavaUtil.java
+++ main/java/uk/co/real_logic/sbe/generation/java/JavaUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.real_logic.sbe.codec.java;
+package uk.co.real_logic.sbe.generation.java;
 
 import uk.co.real_logic.sbe.PrimitiveType;
 
