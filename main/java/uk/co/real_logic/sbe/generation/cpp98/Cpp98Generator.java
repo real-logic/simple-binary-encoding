0cb0989d1fa5c4467b87cbcf6f03d723ef8ef4c7
lcs777 <luke.strobel@gmail.com>
2015-04-25 00:04:50 -0400

    Adding namespace to include guard in order to avoid name collisions.

--- main/java/uk/co/real_logic/sbe/generation/cpp98/Cpp98Generator.java
+++ main/java/uk/co/real_logic/sbe/generation/cpp98/Cpp98Generator.java
@@ -736,8 +736,8 @@ public class Cpp98Generator implements CodeGenerator
         ));
 
         sb.append(String.format(
-            "#ifndef _%1$s_HPP_\n" +
-            "#define _%1$s_HPP_\n\n" +
+            "#ifndef _%1$s_%2$s_HPP_\n" +
+            "#define _%1$s_%2$s_HPP_\n\n" +
             "#if defined(SBE_HAVE_CMATH)\n" +
             "/* cmath needed for std::numeric_limits<double>::quiet_NaN() */\n" +
             "#  include <cmath>\n" +
@@ -750,6 +750,7 @@ public class Cpp98Generator implements CodeGenerator
             "#  define SBE_DOUBLE_NAN NAN\n" +
             "#endif\n\n" +
             "#include <sbe/sbe.hpp>\n\n",
+            namespaceName.toUpperCase(),
             className.toUpperCase()
         ));
 
