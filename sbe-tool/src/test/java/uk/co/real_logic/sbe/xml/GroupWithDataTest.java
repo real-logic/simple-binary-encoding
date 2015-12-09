c0172d3e89006b6523340068067a9229bba6c051
Todd L. Montgomery <tmont@nard.net>
2015-12-08 16:24:56 -0800

    added initial repeating group with vardata test and test schema.

---
+++ sbe-tool/src/test/java/uk/co/real_logic/sbe/xml/GroupWithDataTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.sbe.xml;
+
+import org.junit.Test;
+import uk.co.real_logic.sbe.TestUtil;
+
+import static uk.co.real_logic.sbe.xml.XmlSchemaParser.parse;
+
+public class GroupWithDataTest
+{
+    @Test
+    public void shouldParseSchemaSuccessfully()
+        throws Exception
+    {
+        parse(TestUtil.getLocalResource("group-with-data-schema.xml"), ParserOptions.DEFAULT);
+    }
+}
