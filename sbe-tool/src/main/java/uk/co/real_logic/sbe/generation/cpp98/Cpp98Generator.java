93b97e13eecc8ce993178d435a7298dbb67e95eb
Todd L. Montgomery <tmont@nard.net>
2015-12-15 18:09:45 -0800

    [C++]: add conditional include for C++11 to codecs

--- sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/cpp98/Cpp98Generator.java
+++ sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/cpp98/Cpp98Generator.java
@@ -743,6 +743,9 @@ public class Cpp98Generator implements CodeGenerator
             "#  define SBE_FLOAT_NAN NAN\n" +
             "#  define SBE_DOUBLE_NAN NAN\n" +
             "#endif\n\n" +
+            "#if __cplusplus < 201103L\n" +
+            "#  include <functional>\n" +
+            "#endif\n\n" +
             "#include <sbe/sbe.hpp>\n\n",
             namespaceName.toUpperCase(),
             className.toUpperCase()
