966cc07ad59058d6a059044f1d81cd1cefa017c5
Judd Gaddie <judd.gaddie@transficc.com>
2016-04-20 13:47:55 +0100

    [Java] default output directory for SbeTool to CWD

--- sbe-tool/src/main/java/uk/co/real_logic/sbe/SbeTool.java
+++ sbe-tool/src/main/java/uk/co/real_logic/sbe/SbeTool.java
@@ -171,9 +171,9 @@ public class SbeTool
                 return;
             }
 
+            final String outputDirName = System.getProperty(OUTPUT_DIR, ".");
             if (Boolean.parseBoolean(System.getProperty(GENERATE_STUBS, "true")))
             {
-                final String outputDirName = System.getProperty(OUTPUT_DIR, "");
                 final String targetLanguage = System.getProperty(TARGET_LANGUAGE, "Java");
 
                 generate(ir, outputDirName, targetLanguage);
@@ -187,7 +187,6 @@ public class SbeTool
                 final int nameEnd = inputFilename.lastIndexOf('.');
                 final String namePart = inputFilename.substring(0, nameEnd);
 
-                final String outputDirName = System.getProperty(OUTPUT_DIR, "");
                 final File fullPath = new File(outputDirName, namePart + ".sbeir");
 
                 try (final IrEncoder irEncoder = new IrEncoder(fullPath.getAbsolutePath(), ir))
