58dd3f1dc135a882c1694f05de6e3c72da0d36f0
Xavier Daragon <xavier.daragon@ullink.com>
2016-07-06 18:37:40 +0200

    [Java] Fix empty group display
    Change-Id: I6f20831701069dddcfb40f8702c03f0483f0d01b

--- sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/java/ToStringTest.java
+++ sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/java/ToStringTest.java
@@ -1,5 +1,6 @@
 package uk.co.real_logic.sbe.generation.java;
 
+import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.Test;
 import uk.co.real_logic.sbe.EncodedCarTestBase;
 
@@ -34,6 +35,22 @@ public class ToStringTest extends EncodedCarTestBase
                 "(octaneRating=95|acceleration=[(mph=30|seconds=4.0|),(mph=60|seconds=7.5|),(mph=100|seconds=12.2|)]|)," +
                 "(octaneRating=99|acceleration=[(mph=30|seconds=3.8|),(mph=60|seconds=7.1|),(mph=100|seconds=11.8|)]|)]|" +
                 "make=Honda|model=Civic VTi|activationCode=|",
+                result);
+    }
+
+    @Test
+    public void emptyMessagePrinted() throws Exception
+    {
+        final ByteBuffer encodedMsgBuffer = ByteBuffer.allocateDirect(MSG_BUFFER_CAPACITY);
+        CAR.wrap(new UnsafeBuffer(encodedMsgBuffer), 0);
+
+        final String result = CAR.toString();
+        assertEquals(
+                "[Car]" +
+                "(sbeTemplateId=1|sbeSchemaId=1|sbeSchemaVersion=0|sbeBlockLength=45):" +
+                "serialNumber=0|modelYear=0|available=F|code=NULL_VAL|someNumbers=[0,0,0,0,0]|vehicleCode=|extras={}|" +
+                "engine=(capacity=0|numCylinders=0|manufacturerCode=|)|" +
+                "fuelFigures=[]|performanceFigures=[]|make=|model=|activationCode=|",
                 result);
     }
 }
