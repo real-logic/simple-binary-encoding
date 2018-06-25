6d1c731ebf18101a1e825f6d440a3823cf00fd59
Zach Bray <zachbray@googlemail.com>
2018-06-25 10:35:10 +0100

    [C#] Add failing test around throwing to indicate that group size representation is invalid.

---
+++ sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/csharp/Issue567GroupSizeTest.java
@@ -0,0 +1,34 @@
+package uk.co.real_logic.sbe.generation.csharp;
+
+import static uk.co.real_logic.sbe.xml.XmlSchemaParser.parse;
+
+import org.agrona.generation.StringWriterOutputManager;
+import org.junit.Test;
+
+import uk.co.real_logic.sbe.TestUtil;
+import uk.co.real_logic.sbe.ir.Ir;
+import uk.co.real_logic.sbe.xml.IrGenerator;
+import uk.co.real_logic.sbe.xml.MessageSchema;
+import uk.co.real_logic.sbe.xml.ParserOptions;
+
+public class Issue567GroupSizeTest
+{
+
+    @Test(expected = Exception.class)
+    public void shouldThrowWhenUsingATypeThatIsNotConstrainedToFitInAnIntAsTheGroupSize() throws Exception
+    {
+        // Arrange
+        final ParserOptions options = ParserOptions.builder().stopOnError(true).build();
+        final MessageSchema schema = parse(TestUtil.getLocalResource("issue567-invalid.xml"), options);
+        final IrGenerator irg = new IrGenerator();
+        final Ir ir = irg.generate(schema);
+
+        final StringWriterOutputManager outputManager = new StringWriterOutputManager();
+        outputManager.setPackageName(ir.applicableNamespace());
+        final CSharpGenerator generator = new CSharpGenerator(ir, outputManager);
+
+        // Act + Assert
+        generator.generate();
+    }
+
+}
