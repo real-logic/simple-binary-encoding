e4a26c724f5f09836d42a81a6b08bc0481d36a52
mjpt777 <mjpt777@gmail.com>
2018-06-25 15:11:13 +0100

    [Java] Suppress warning output but verify it. PR #568.

--- sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/csharp/Issue567GroupSizeTest.java
+++ sbe-tool/src/test/java/uk/co/real_logic/sbe/generation/csharp/Issue567GroupSizeTest.java
@@ -1,9 +1,11 @@
 package uk.co.real_logic.sbe.generation.csharp;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static uk.co.real_logic.sbe.xml.XmlSchemaParser.parse;
 
 import org.agrona.generation.StringWriterOutputManager;
-import org.junit.Test;
+import org.junit.*;
 
 import uk.co.real_logic.sbe.TestUtil;
 import uk.co.real_logic.sbe.ir.Ir;
@@ -11,12 +13,33 @@ import uk.co.real_logic.sbe.xml.IrGenerator;
 import uk.co.real_logic.sbe.xml.MessageSchema;
 import uk.co.real_logic.sbe.xml.ParserOptions;
 
+import java.io.PrintStream;
+
 public class Issue567GroupSizeTest
 {
+    public static final String ERR_MSG =
+        "WARNING: at <sbe:message name=\"issue567\"> <group name=\"group\"> \"numInGroup\" should be UINT8 or UINT16";
+
+    private PrintStream err;
+    private PrintStream mockErr = mock(PrintStream.class);
+
+    @Before
+    public void before()
+    {
+        err = System.err;
+        System.setErr(mockErr);
+    }
+
+    @After
+    public void after()
+    {
+        System.setErr(err);
+        verify(mockErr).println(ERR_MSG);
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowWhenUsingATypeThatIsNotConstrainedToFitInAnIntAsTheGroupSize() throws Exception
     {
-        // Arrange
         final ParserOptions options = ParserOptions.builder().stopOnError(true).build();
         final MessageSchema schema = parse(TestUtil.getLocalResource("issue567-invalid.xml"), options);
         final IrGenerator irg = new IrGenerator();
@@ -33,7 +56,6 @@ public class Issue567GroupSizeTest
     @Test
     public void shouldGenerateWhenUsingATypeThatIsConstrainedToFitInAnIntAsTheGroupSize() throws Exception
     {
-        // Arrange
         final ParserOptions options = ParserOptions.builder().stopOnError(true).build();
         final MessageSchema schema = parse(TestUtil.getLocalResource("issue567-valid.xml"), options);
         final IrGenerator irg = new IrGenerator();
