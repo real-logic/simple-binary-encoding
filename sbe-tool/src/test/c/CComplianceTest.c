c8614b6b0743e7d8f9467088eb9883f3c9df8b45
Deniz Evrenci <denizevrenci@gmail.com>
2019-02-01 16:22:45 +0900

    [C] Add a dummy source to check compliance.
    Since the tests are still C++ sources, the C headers are compiled with a
    C++ compiler. This makes it difficult to notice breaking C
    compatibility.

---
+++ sbe-tool/src/test/c/CComplianceTest.c
@@ -0,0 +1,10 @@
+#include <code_generation_test/car.h>
+#include <code_generation_test/messageHeader.h>
+
+#include <group_with_data/testMessage1.h>
+#include <group_with_data/testMessage2.h>
+#include <group_with_data/testMessage3.h>
+#include <group_with_data/testMessage4.h>
+
+int main() {
+}
