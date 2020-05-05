a62d9e39c12d56743027aadc5cb9caf45ade48df
Stephan Zehetner <stephan.zehetner@gmail.com>
2020-05-05 16:19:47 +0200

    add SchemaVersion to MessageHeader

--- sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/csharp/CSharpGenerator.java
+++ sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/csharp/CSharpGenerator.java
@@ -1029,7 +1029,12 @@ public class CSharpGenerator implements CodeGenerator
 
     private CharSequence generateFixedFlyweightCode(final int size)
     {
+        final String schemaIdType = cSharpTypeName(ir.headerStructure().schemaIdType());
+        final String schemaVersionType = cSharpTypeName(ir.headerStructure().schemaVersionType());
+
         return String.format(
+            INDENT + INDENT + "public const %1$s SchemaId = %2$s;\n" +
+            INDENT + INDENT + "public const %3$s SchemaVersion = %4$s;\n\n" +
             INDENT + INDENT + "private DirectBuffer _buffer;\n" +
             INDENT + INDENT + "private int _offset;\n" +
             INDENT + INDENT + "private int _actingVersion;\n\n" +
@@ -1039,7 +1044,11 @@ public class CSharpGenerator implements CodeGenerator
             INDENT + INDENT + INDENT + "_actingVersion = actingVersion;\n" +
             INDENT + INDENT + INDENT + "_buffer = buffer;\n" +
             INDENT + INDENT + "}\n\n" +
-            INDENT + INDENT + "public const int Size = %d;\n",
+            INDENT + INDENT + "public const int Size = %5$d;\n",
+            schemaIdType,
+            generateLiteral(ir.headerStructure().schemaIdType(), Integer.toString(ir.id())),
+            schemaVersionType,
+            generateLiteral(ir.headerStructure().schemaVersionType(), Integer.toString(ir.version())),
             size);
     }
 
