db4eca423816ad1d5376b90c15407c80130b35db
Yonggang Luo <luoyonggang@gmail.com>
2020-05-27 07:54:15 +0800

    Windows CI fixes.

--- cppbuild/cppbuild.cmd
+++ cppbuild/cppbuild.cmd
@@ -1,5 +1,6 @@
 @if "%DEBUG%" == "" @echo off
 setlocal EnableDelayedExpansion
+cd "/d %~dp0.."
 
 set SOURCE_DIR=%CD%
 set BUILD_DIR=%CD%\cppbuild\Release
@@ -12,6 +13,7 @@ if EXIST %BUILD_DIR% rd /S /Q %BUILD_DIR%
 md %BUILD_DIR%
 pushd %BUILD_DIR%
 
+cmake -G "Ninja" %SOURCE_DIR%
 cmake --build . --clean-first --config Release
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
