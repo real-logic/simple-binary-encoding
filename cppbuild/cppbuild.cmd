82596e02b4b330467b1a8ba13a0160e766436a3c
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2020-05-27 00:14:24 +0200

    [C++] Add Windows build to the CI matrix

--- cppbuild/cppbuild.cmd
+++ cppbuild/cppbuild.cmd
@@ -1,14 +1,19 @@
 @if "%DEBUG%" == "" @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 set SOURCE_DIR=%CD%
 set BUILD_DIR=%CD%\cppbuild\Release
+
+call cppbuild/vs-helper.cmd
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 if EXIST %BUILD_DIR% rd /S /Q %BUILD_DIR%
 
 md %BUILD_DIR%
 pushd %BUILD_DIR%
 
-cmake -G "Visual Studio 14 Win64" %SOURCE_DIR%
 cmake --build . --clean-first --config Release
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 ctest -C Release
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
