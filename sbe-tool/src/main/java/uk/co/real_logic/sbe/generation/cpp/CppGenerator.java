84066e15c86d1974b2dc1ac4f6e96a51cca5ee1a
Odysseas Georgoudis <odygrd@gmail.com>
2024-02-03 14:56:25 +0000

    [C++] make message header members constexpr

--- sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/cpp/CppGenerator.java
+++ sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/cpp/CppGenerator.java
@@ -2673,10 +2673,10 @@ public class CppGenerator implements CodeGenerator
             "%22$s" +
 
             "public:\n" +
-            "    static const %1$s SBE_BLOCK_LENGTH = %2$s;\n" +
-            "    static const %3$s SBE_TEMPLATE_ID = %4$s;\n" +
-            "    static const %5$s SBE_SCHEMA_ID = %6$s;\n" +
-            "    static const %7$s SBE_SCHEMA_VERSION = %8$s;\n" +
+            "    static constexpr %1$s SBE_BLOCK_LENGTH = %2$s;\n" +
+            "    static constexpr %3$s SBE_TEMPLATE_ID = %4$s;\n" +
+            "    static constexpr %5$s SBE_SCHEMA_ID = %6$s;\n" +
+            "    static constexpr %7$s SBE_SCHEMA_VERSION = %8$s;\n" +
             "    static constexpr const char* SBE_SEMANTIC_VERSION = \"%13$s\";\n\n" +
 
             "    enum MetaAttribute\n" +
