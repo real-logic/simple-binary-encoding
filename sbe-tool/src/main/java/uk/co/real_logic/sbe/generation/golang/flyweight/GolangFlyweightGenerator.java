91813eafd488ee47f43054f8efcd63ab1ab00249
mjpt777 <mjpt777@gmail.com>
2024-12-11 12:02:05 +0000

    Fix warning about used args in GolangFlyweightGenerator.

--- sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/golang/flyweight/GolangFlyweightGenerator.java
+++ sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/golang/flyweight/GolangFlyweightGenerator.java
@@ -2236,7 +2236,7 @@ public class GolangFlyweightGenerator implements CodeGenerator
             "        %10$sSbeTemplateID      %3$s = %4$s\n" +
             "        %10$sSbeSchemaID        %5$s = %6$s\n" +
             "        %10$sSbeSchemaVersion   %7$s = %8$s\n" +
-            "        %10$sSbeSemanticVersion string = \"%13$s\"\n" +
+            "        %10$sSbeSemanticVersion string = \"%11$s\"\n" +
             "    )\n\n" +
 
             "    func (m *%10$s) SbeBlockLength() %1$s {\n" +
@@ -2260,7 +2260,7 @@ public class GolangFlyweightGenerator implements CodeGenerator
             "    }\n\n" +
 
             "    func (m *%10$s) SbeSemanticVersion() string {\n" +
-            "        return \"%13$s\"\n" +
+            "        return \"%11$s\"\n" +
             "    }\n\n" +
 
             "    func (m *%10$s) SbeSemanticType() string {\n" +
@@ -2375,9 +2375,8 @@ public class GolangFlyweightGenerator implements CodeGenerator
             generateLiteral(ir.headerStructure().schemaVersionType(), Integer.toString(ir.version())),
             semanticType,
             className,
-            generateConstructorsAndOperators(className),
-            formatClassName(headerType),
-            semanticVersion));
+            generateConstructorsAndOperators(className)));
+
         return sb.toString();
     }
 
