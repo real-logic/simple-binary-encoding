658fcbd5fc48178b1bad22aec2dbb3191fef54dc
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-12-17 15:54:01 +0100

    [Go] Fix Golang build.

--- gocode/flyweight/src/otf/otf_test.go
+++ gocode/flyweight/src/otf/otf_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-//go:generate java -Dsbe.output.dir=. -Dsbe.target.language=golang -Dsbe.go.generate.generate.flyweights=true -Dsbe.generate.ir=true -jar ../../../${SBE_JAR} ../../../../sbe-tool/src/test/resources/code-generation-schema.xml
+//go:generate java --add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dsbe.output.dir=. -Dsbe.target.language=golang -Dsbe.go.generate.generate.flyweights=true -Dsbe.generate.ir=true -jar ../../../${SBE_JAR} ../../../../sbe-tool/src/test/resources/code-generation-schema.xml
 
 const SchemaFilename = "code-generation-schema.sbeir"
 const MsgBufferCapacity = 4 * 1024
